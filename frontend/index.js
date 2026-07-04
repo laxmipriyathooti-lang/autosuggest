354fe356948147d6b406d0de5f991c08b173799a
laxmi priya thooti <laxmipriyathooti@gmail.com>
2026-07-04 09:59:21 +0530

    Random User

--- frontend/index.js
+++ frontend/index.js
@@ -23,5 +23,29 @@ function toggle(){
   //gender
   document.getElementById("card-gender").innerText=users[curUserId].gender;
 }
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name
+      // img
+      document.getElementById("img").src=imageUrl
+
+    })
+}
 
 console.log("Hello from JS");
